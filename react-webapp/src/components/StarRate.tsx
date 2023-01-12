30cb8266cb082c8f41751832bc5a01e5cdbc417f
sandrewtx08 <sandrewtx08@protonmail.com>
2023-01-12 17:49:10 -0300

    refactor(starsreview): swich case

--- react-webapp/src/components/StarRate.tsx
+++ react-webapp/src/components/StarRate.tsx
@@ -2,53 +2,70 @@ import { Fragment } from "react";
 import { RiStarLine, RiStarFill } from "react-icons/ri";
 
 export default function (props: { rate: number }) {
-  return props.rate.toPrecision()[0] == "1" ? (
-    <Fragment>
-      <RiStarFill />
-      <RiStarLine />
-      <RiStarLine />
-      <RiStarLine />
-      <RiStarLine />
-    </Fragment>
-  ) : props.rate.toPrecision()[0] == "2" ? (
-    <Fragment>
-      <RiStarFill />
-      <RiStarFill />
-      <RiStarLine />
-      <RiStarLine />
-      <RiStarLine />
-    </Fragment>
-  ) : props.rate.toPrecision()[0] == "3" ? (
-    <Fragment>
-      <RiStarFill />
-      <RiStarFill />
-      <RiStarFill />
-      <RiStarLine />
-      <RiStarLine />
-    </Fragment>
-  ) : props.rate.toPrecision()[0] == "4" ? (
-    <Fragment>
-      <RiStarFill />
-      <RiStarFill />
-      <RiStarFill />
-      <RiStarFill />
-      <RiStarLine />
-    </Fragment>
-  ) : props.rate.toPrecision()[0] == "5" ? (
-    <Fragment>
-      <RiStarFill />
-      <RiStarFill />
-      <RiStarFill />
-      <RiStarFill />
-      <RiStarFill />
-    </Fragment>
-  ) : (
-    <Fragment>
-      <RiStarLine />
-      <RiStarLine />
-      <RiStarLine />
-      <RiStarLine />
-      <RiStarLine />
-    </Fragment>
-  );
+  switch (props.rate.toPrecision()[0]) {
+    case "1":
+      return (
+        <Fragment>
+          <RiStarFill />
+          <RiStarLine />
+          <RiStarLine />
+          <RiStarLine />
+          <RiStarLine />
+        </Fragment>
+      );
+
+    case "2":
+      return (
+        <Fragment>
+          <RiStarFill />
+          <RiStarFill />
+          <RiStarLine />
+          <RiStarLine />
+          <RiStarLine />
+        </Fragment>
+      );
+
+    case "3":
+      return (
+        <Fragment>
+          <RiStarFill />
+          <RiStarFill />
+          <RiStarFill />
+          <RiStarLine />
+          <RiStarLine />
+        </Fragment>
+      );
+
+    case "4":
+      return (
+        <Fragment>
+          <RiStarFill />
+          <RiStarFill />
+          <RiStarFill />
+          <RiStarFill />
+          <RiStarLine />
+        </Fragment>
+      );
+    case "5":
+      return (
+        <Fragment>
+          <RiStarFill />
+          <RiStarFill />
+          <RiStarFill />
+          <RiStarFill />
+          <RiStarFill />
+        </Fragment>
+      );
+
+    default:
+      return (
+        <Fragment>
+          <RiStarLine />
+          <RiStarLine />
+          <RiStarLine />
+          <RiStarLine />
+          <RiStarLine />
+        </Fragment>
+      );
+  }
 }
