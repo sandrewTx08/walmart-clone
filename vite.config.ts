e733af393e3e8c8fc1cae8398e2c6b1191f7d1d3
sandrewtx08 <sandrewtx08@protonmail.com>
2023-01-16 16:50:01 -0300

    fix: build & public folder path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { join } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "client",
-  build: { outDir: "dist/client" },
+  root: join(__dirname, "client"),
+  publicDir: join(__dirname, "public"),
+  build: { outDir: join(__dirname, "dist", "client") },
   plugins: [react()],
 });
