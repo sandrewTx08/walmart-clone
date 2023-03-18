4870b6dc6a3e903221e4da008d76e1b95f9a8b6f
sandrewtx08 <sandrewtx08@protonmail.com>
2023-03-17 23:31:19 -0300

    style: home layout

--- components/Home/Home1.tsx
+++ components/Home/Home1.tsx
@@ -1,76 +1,144 @@
+import Link from "next/link";
 import styled from "styled-components";
+import Card from "@/components/Product/Card1";
 
 const Container = styled.div`
   justify-content: center;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, auto));
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 340px 1fr;
+  gap: 1em;
+
+  @media only screen and (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 1fr;
+  }
 `;
 
 const Img = styled.img`
   display: block;
   height: auto;
   width: 100%;
-  margin-bottom: 1em;
-  border-radius: 1em;
+  margin: 1em 0;
+  border-radius: 50%;
 `;
 
 const Wrapper = styled.div`
+  display: flex;
   padding: 2em;
-  margin: 1em;
   border-radius: 1em;
+  flex-direction: column;
 `;
 
-export default function C() {
+export default function C({ product }) {
   return (
     <Container>
-      <Wrapper className="shadow-soft">
-        <Img src="https://picsum.photos/seed/465/300" />
-        <h2>Lorem ipsum dolor sit amet.</h2>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magni
+      <Wrapper
+        className="shadow-soft"
+        style={{
+          gridColumnStart: 1,
+          gridColumnEnd: 3,
+          backgroundImage: 'url("https://picsum.photos/seed/700/400")',
+          backgroundPosition: "center",
+          fontWeight: "bolder",
+          fontSize: "xx-large",
+          color: "white",
+          opacity: ".8",
+        }}
+      >
+        Buy online now the best products
       </Wrapper>
 
+      <div style={{ margin: "auto" }}>
+        <Card product={product} />
+      </div>
+
       <Wrapper className="shadow-soft">
-        <Img src="https://picsum.photos/seed/556/300" />
-        <h2>Lorem ipsum dolor sit amet.</h2>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magni
-        delectus, dolore
+        <div>
+          <h3>Top departments</h3>
+
+          <Link href="/all-departments">See all</Link>
+
+          <div style={{ display: "flex", gap: 12, margin: "auto 0" }}>
+            <Img src="https://picsum.photos/seed/562/300" />
+            <Img src="https://picsum.photos/seed/654742/300" />
+            <Img src="https://picsum.photos/seed/567657/300" />
+          </div>
+        </div>
+
+        <div>
+          <h3>Trend departments</h3>
+
+          <Link href="/all-departments">See all</Link>
+
+          <div style={{ display: "flex", gap: 12, margin: "auto 0" }}>
+            <Img src="https://picsum.photos/seed/456/300" />
+            <Img src="https://picsum.photos/seed/167/300" />
+            <Img src="https://picsum.photos/seed/231/300" />
+          </div>
+        </div>
       </Wrapper>
 
-      <Container>
-        <Wrapper className="shadow-soft">
-          <Img src="https://picsum.photos/seed/654/300" />
-          <h2>Lorem ipsum dolor sit amet.</h2>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magni
-          delectus, dolore
-        </Wrapper>
+      <Wrapper
+        className="shadow-soft"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          gap: 12,
+          fontSize: "smaller",
+          textAlign: "center",
+        }}
+      >
+        <div>
+          <Img src="https://picsum.photos/seed/33432/300" />
+          Sweet price
+        </div>
 
-        <Wrapper className="shadow-soft">
-          <Img src="https://picsum.photos/seed/5642/300" />
-          <h2>Lorem ipsum dolor sit amet.</h2>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magni
-          delectus, dolore
-        </Wrapper>
-      </Container>
+        <div>
+          <Img src="https://picsum.photos/seed/974358978/300" />
+          Discount 80%
+        </div>
 
-      <Wrapper className="shadow-soft">
-        <Img src="https://picsum.photos/seed/769/300" />
-        <h2>Lorem ipsum dolor sit amet.</h2>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magni
-        delectus, dolore
+        <div>
+          <Img src="https://picsum.photos/seed/7638/300" />
+          Save 1$ for each item
+        </div>
+
+        <div>
+          <Img src="https://picsum.photos/seed/213/300" />
+          Digit the cupom for discount
+        </div>
       </Wrapper>
 
-      <Wrapper className="shadow-soft">
-        <Img src="https://picsum.photos/seed/854/300" />
-        <h2>Lorem ipsum dolor sit amet.</h2>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magni
-        delectus, dolore
-      </Wrapper>
+      <Wrapper
+        className="shadow-soft"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          gap: 12,
+          fontSize: "smaller",
+          textAlign: "center",
+        }}
+      >
+        <div>
+          <Img src="https://picsum.photos/seed/65478/300" />
+          Gift for a friend
+        </div>
 
-      <Wrapper className="shadow-soft">
-        <Img src="https://picsum.photos/seed/7809/300" />
-        <h2>Lorem ipsum dolor sit amet.</h2>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae magni
-        delectus, dolore
+        <div>
+          <Img src="https://picsum.photos/seed/8689/300" />
+          Holidays gift
+        </div>
+
+        <div>
+          <Img src="https://picsum.photos/seed/9885/300" />
+          Special discount on card
+        </div>
+
+        <div>
+          <Img src="https://picsum.photos/seed/566/300" />
+          Digit the cupom for discount
+        </div>
       </Wrapper>
     </Container>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
 import Home from "@/components/Home/Home1";
+import prisma from "@/utils/prismaClient";
 
-export default function C() {
-  return <Home />;
+export async function getServerSideProps() {
+  const data = await prisma.products.findFirst({
+    include: { ProductPhotos: { include: { Photos: true } } },
+  });
+
+  return {
+    props: { product: { ...data, price: data.price.toNumber() } },
+  };
+}
+
+export default function C({ product }) {
+  return <Home product={product} />;
 }
